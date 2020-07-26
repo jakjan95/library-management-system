f85838df483b4c8fe529086298e779d60aa53f35
jakjan95 <jakubjanusz95@gmail.com>
2020-07-26 17:43:27 +0200

    book class changed into mvp version

--- include/book.h
+++ include/book.h
@@ -17,10 +17,6 @@ public:
     Book(std::string title,
          std::string isbn,
          std::string author,
-         std::string publisher,
-         size_t publishedYear,
-         size_t edition,
-         size_t pagesNumbers,
          Field field,
          bool available);
 
@@ -29,23 +25,16 @@ public:
     std::string getTitle() const { return title_; }
     std::string getIsbn() const { return isbn_; }
     std::string getAuthor() const { return author_; }
-    std::string getPublisher() const { return publisher_; }
-    size_t getPublishedYear() const { return publishedYear_; }
-    size_t getEdition() const { return edition_; }
-    size_t getPageNumbers() const { return pagesNumbers_; }
     Field getField() const { return field_; }
     bool getAvailable() const { return available_; }
 
-    void setAvailableStatus();
+    void borrowBook();
+    void returnBook();
 
 private:
     std::string title_;
     std::string isbn_;
     std::string author_;
-    std::string publisher_;
-    size_t publishedYear_;
-    size_t edition_;
-    size_t pagesNumbers_;
     Field field_;
     bool available_;
 };
--- src/book.cpp
+++ src/book.cpp
@@ -7,44 +7,32 @@
 Book::Book(std::string title,
            std::string isbn,
            std::string author,
-           std::string publisher,
-           size_t publishedYear,
-           size_t edition,
-           size_t pagesNumbers,
            Field field,
            bool available)
     : title_{title},
       isbn_{isbn},
       author_{author},
-      publisher_{publisher},
-      publishedYear_{publishedYear},
-      edition_{edition},
-      pagesNumbers_{pagesNumbers},
       field_{field},
       available_{available} {}
 
-
-    void Book::setAvailableStatus() {
-        if (getAvailable()) {
-            std::cout<<"Renting a book\n";
-            available_ = false;
-        } else {
-            std::cout<<"Returning a book\n";
-            available_ = true;
-        }
+void Book::borrowBook() {
+    if (getAvailable()) {
+        std::cout << "Renting a book\n";
+        available_ = false;
     }
-
-
+}
+void Book::returnBook() {
+    if (!getAvailable()) {
+        std::cout << "Returning a book\n";
+        available_ = true;
+    }
+}
 
 std::ostream& operator<<(std::ostream& os, const Book& b) {
-    //|Title | ISBN | Author | Publisher | Published Year | Edition | Page Numbers | Field | Available |
+    //|Title | ISBN | Author | Field | Available |
     os << '|' << b.getTitle() << std::setw(14) << '|'
        << b.getIsbn() << std::setw(14) << '|'
        << b.getAuthor() << std::setw(14) << '|'
-       << b.getPublisher() << std::setw(14) << '|'
-       << b.getPublishedYear() << std::setw(6) << '|'
-       << b.getEdition() << std::setw(2) << '|'
-       << b.getPageNumbers() << std::setw(4) << '|'
        << getFieldName(b.getField()) << std::setw(4) << '|'
        << std::boolalpha << b.getAvailable() << std::setw(4) << '|';
     return os;
